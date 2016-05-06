7955d58575aca6705a0cef91207da47d28ea72a3
riandreslemos1997 <riandreslemos1997@gmail.com>
2016-05-06 10:26:32 -0300

    Update ScriptSO.sh

--- ScriptSO.sh
+++ ScriptSO.sh
@@ -1,5 +1,18 @@
 !#/bin/bash
-1)Alta Usuario
-2)Baja Usuario
-3)Modificacion de Usuario
-4)Salir
+echo 1- Buscar usuario
+echo 2- Buscar grupo
+echo 3- Modificacion home de Usuario
+echo 4- Modificar GID de grupo
+echo 4- Salir
+
+read selectOpcion
+case  $SelectOpcion in
+ 1)  echo 1- Buscar usuario
+     echo 2- Atras
+     read $Selecion1
+     while ($Selecion1 = 2)
+      1) 
+     
+     
+     done
+esac
